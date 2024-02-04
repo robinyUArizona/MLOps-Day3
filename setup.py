c2cd19a020d747085992cf42274ce497ca16492d
robinyUArizona <robiny@arizona.edu>
2024-02-04 15:40:48 +0545

    adding all again forpublish

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.md', 'r', encoding="ISO-8859-1") as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.1"
+__version__ = "0.0.4"
 REPO_NAME = "MLOps-Day3-mongodbconnectorpkg"
 PKG_NAME= "databaseautomation"
 AUTHOR_USER_NAME = "robinyUArizona"
